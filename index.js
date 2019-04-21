53060dc4e6726f636ddb2d80011aacf9c0a0b7a5
Owais Shah <owaisshah110@yahoo.com>
2019-04-21 20:08:41 +0500

    in class

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './src/components/App'
 import {name as appName} from './app.json';
+export * from "./CustomButton";
+export * from "./Card";
+export * from "./CardSection";
+export * from "./Header";
+export * from "./Input";
 
 AppRegistry.registerComponent(appName, () => App);
